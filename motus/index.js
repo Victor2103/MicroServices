3179e1f6f8d9d63854271be7ca7682094b5485e6
Victor_2103 <vitcheffvi@cy-tech.fr>
2022-09-22 16:08:58 +0200

    je push

--- motus/index.js
+++ motus/index.js
@@ -2,45 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 var fs = require("fs");
-
-date_ob = new Date();
-
-// current date
-// adjust 0 before single digit date
-date = ("0" + date_ob.getDate()).slice(-2);
-
-// current month
-month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-
-// current year
-year = date_ob.getFullYear();
-
-
-
-
-const content = date+"/"+month+"/"+year;
-var datecourante = fs
-  .readFileSync(__dirname + "/data/test.txt")
-  .toString()
-  .split("\n");
-
-if (datecourante[0]===content){
-  const motmystere=datecourante[1]
-  console.log(motmystere)
-} else {
-  fs.writeFile(__dirname+'/data/test.txt', content, err => {
-    if (err) {
-      console.error(err);
-    }
-    // file written successfully
-  });
-}
-
-
-
-
-
-
+app.use(express.static("www"))
 var array = fs
   .readFileSync(__dirname + "/data/liste_francais.txt")
   .toString()
@@ -51,7 +13,40 @@ app.get("/", (req, res) => {
 });
 
 app.get("/word", (req, res) => {
-  res.send(array[127]);
+  date_ob = new Date();
+
+  // current date
+  // adjust 0 before single digit date
+  date = ("0" + date_ob.getDate()).slice(-2);
+
+  // current month
+  month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+  // current year
+  year = date_ob.getFullYear();
+
+  const content = date + "/" + month + "/" + year;
+  var datecourante = fs
+    .readFileSync(__dirname + "/data/test.txt")
+    .toString()
+    .split("\n");
+
+  if (datecourante[0] === content) {
+  } else {
+    fs.writeFile(
+      __dirname + "/data/test.txt",
+      content + "\n" + Math.floor(Math.random() * 22739),
+      (err) => {
+        if (err) {
+          console.error(err);
+        }
+        // file written successfully
+      }
+    );
+  }
+
+  const indicemotmystere = datecourante[1];
+  res.send(array[indicemotmystere]);
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
