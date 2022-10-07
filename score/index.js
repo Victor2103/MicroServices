56bccc3864d8c3222d06585ed32ba8aa49917746
Victor_2103 <vitcheffvi@cy-tech.fr>
2022-10-07 13:41:14 +0200

    try local storage with score

--- score/index.js
+++ score/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
+const store = require("store");
 const port = process.env.PORT ||4000 
 
 app.get("/", (req, res) => {
     res.send("Hello World !");
   });
+
+app.get("/",(req,res)=> {
+    // Store current user 
+store.set('user', { name:'Marcus' })
+
+// Get current user 
+store.get('user')
+
+
+// Loop over all stored values 
+store.each(function(value, key) {
+    console.log(key, '==', value)
+})
+res.send("Check console")
+})
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
