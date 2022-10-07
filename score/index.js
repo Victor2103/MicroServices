dbc9abf86e844ec976d4d1a0c7a3966bbb1f7e65
Victor_2103 <vitcheffvi@cy-tech.fr>
2022-10-07 14:55:06 +0200

    use static fles for score

--- score/index.js
+++ score/index.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const app = express();
-const store = require("store");
+const store = require("store2");
 const port = process.env.PORT ||4000 
+const path = require('path')
+
+app.use(express.static("www"))
 
 app.get("/", (req, res) => {
     res.send("Hello World !");
   });
 
-app.get("/",(req,res)=> {
-    // Store current user 
-store.set('user', { name:'Marcus' })
+app.get("/score",(req,res)=> {
+    // Setting store key and data
+store('Score', {name: 'Adam', age: 27, salary: 3452}); 
+store.setAll({name: 'Adam', age: 34});
+console.log(store("name"))
 
-// Get current user 
-store.get('user')
+});
 
 
-// Loop over all stored values 
-store.each(function(value, key) {
-    console.log(key, '==', value)
-})
-res.send("Check console")
-})
+app.use('/static', express.static(path.join(__dirname, 'www')))
+
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
