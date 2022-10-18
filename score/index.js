55402a28680e338919f9317038b2471673a9b327
Victor_2103 <vitcheffvi@cy-tech.fr>
2022-10-18 10:30:51 +0200

    add nodemon to score app

--- score/index.js
+++ score/index.js
@@ -1,31 +1,28 @@
 const express = require("express");
 const app = express();
 const store = require("store2");
-const port = process.env.PORT ||4000 
-const path = require('path');
-const session = require('cookie-session');
+const port = process.env.PORT || 4000;
+const path = require("path");
 
 app.use(express.static("www"));
 
-
-
 app.get("/", (req, res) => {
-    res.send("Hello World !");
-  });
-
-app.get("/score",(req,res)=> {
-    // Setting store key and data
-store('Score', {name: 'Adam', age: 27, salary: 3452}); 
-store.setAll({name: 'Adam', age: 34});
-console.log(store("name"))
-
+  res.send("Hello World !");
 });
 
-
-app.use('/static', express.static(path.join(__dirname, 'www')))
-
-
-
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.get("/score", (req, res) => {
+  // Setting store key and data
+  store.set("user", { name: "Adam", age: 34 });
+  store.set("user2", { name: "user1", age: 35 });
+  store.each(function (value, key) {
+    console.log(key, "==", value);
+    console.log(typeof key);
+    console.log(typeof value);
   });
+});
+
+app.use("/static", express.static(path.join(__dirname, "www")));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
