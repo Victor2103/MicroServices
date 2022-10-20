fc6ba782a97d567715ad6ec56965e1ca7e2589ec
dufouralex <dufouralex@cy-tech.fr>
2022-10-20 16:40:51 +0200

    update

--- motus/index.js
+++ motus/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 8080
 const os = require("os");
 var fs = require("fs");
 app.use(express.static("www"));
@@ -9,6 +9,20 @@ var array = fs
   .readFileSync(__dirname + "/data/liste_francais_utf8.txt")
   .toString()
   .split("\n");
+
+//Loki pour les logs  
+const loki_uri = process.env.LOKI || "http://127.0.0.1:3100";
+
+const { createLogger, transports } = require("winston");
+const LokiTransport = require("winston-loki");
+const options = {
+  transports: [
+    new LokiTransport({
+      host: loki_uri
+    })
+  ]
+};
+const logger = createLogger(options);
 
 function Verification_mot() {
   var fichier = fs.readFileSync(__dirname+'/data/mot.json')
@@ -22,11 +36,11 @@ function Verification_mot() {
   // current year
   year = date_ob.getFullYear();
   var date_jour = day + "/" + month + "/" + year;
-  var date_mot = mot.date
-  if (date_jour === date_mot) {
-
+  if (date_jour === mot.date) {
+    //ne rien faire
   } else {
     mot.id = Math.floor(Math.random()*22739)
+    mot.date = date_jour
   }
   let donnees = JSON.stringify(mot)
   fs.writeFileSync(__dirname+'/data/mot.json', donnees)
@@ -40,6 +54,8 @@ function get_mot() {
 app.get('/', (req, res) => {
   Verification_mot();
   res.sendFile(__dirname+'/www/home.html')
+  logger.info({ message: 'URL '+req.url , labels: { 'url': req.url, 'user':'test' } });
+  logger.debug({ message: 'test', labels: { 'key': 'value' } });
 })
 
 app.get("/port",(req,res)=>{
@@ -69,5 +85,4 @@ app.get('/word', (req,res)=>{
 app.get('/score', (req, res)=>{
   res.sendFile(__dirname+'/www/score.html')
 })
-
 
