240a5f8ce114fcb94014a5e75d1341a3ebbfcae7
dufouralex <dufouralex@cy-tech.fr>
2022-10-21 14:58:35 +0200

    modif secret key dans le .env

--- motus/index.js
+++ motus/index.js
@@ -52,7 +52,7 @@ var array = fs
 const oneDay = 1000 * 60 * 60 * 24;
 app.use(
   sessions({
-    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    secret: process.env.SECRET_KEY,
     saveUninitialized: true,
     cookie: { maxAge: oneDay },
     resave: false,
