648393438387bf6e0e834617db21d58101541429
Victor_2103 <vitcheffvi@cy-tech.fr>
2022-10-21 18:20:21 +0200

    update score

--- score/index.js
+++ score/index.js
@@ -3,7 +3,6 @@ const app = express();
 const store = require("store2");
 const port = process.env.PORT || 4000;
 const path = require("path");
-const jwt = require("jsonwebtoken");
 const cors = require("cors");
 var userScore;
 
@@ -20,45 +19,23 @@ app.get("/", (req, res) => {
   res.send("Hello World !");
 });
 
-app.post("/score", (req, res) => {
-  console.log(req.body.token);
-  jwt.verify(req.body.token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    console.log(user);
-    if (err) {
-      return res.sendStatus(401);
-    } else {
-      console.log(user);
-      var fichier = fs.readFileSync(__dirname + "/data/mot.json");
-      var mot = JSON.parse(fichier);
-      store.each((value, key) => {
-        if (value === user["username"]) {
-        }
-      });
-    }
-  });
-  store.set("user", { name: "Adam", age: 34 });
-  store.set("user1", { name: "user1", age: 35 });
-  res.status(201).json({
-    ok: "ok",
-  });
-});
-
-app.get("score", (req, res) => {
-  if (req.query.token) {
-    var fichier = fs.readFileSync(__dirname + "/data/mot.json");
+app.get("/update", (req, res) => {
+  if (req.body.username) {
+    var fichier = fs.readFileSync(__dirname + "/score.json");
     var all = JSON.parse(fichier);
-    jwt.verify(
-      req.query.token,
-      process.env.ACCESS_TOKEN_SECRET,
-      (err, user) => {
-        if (err) {
-          res.sendStatus(401);
-        } else {
-        }
+    all.array.forEach((element) => {
+      if (element.username === req.body.username) {
+        element.score = element.score += req.body.score;
       }
-    );
+    });
+    fs.writeFileSync(__dirname + "/score.json", all);
+    next();
+  } else {
+    res.send("non authorize");
   }
 });
+
+app.get("/score", (req, res) => {});
 
 app.use("/static", express.static(path.join(__dirname, "www")));
 
