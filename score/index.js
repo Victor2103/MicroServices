0c85d1569c60985093c1ec2840bf0d906a80de50
Victor_2103 <vitcheffvi@cy-tech.fr>
2022-10-21 16:27:13 +0200

    change nodemon and user session

--- score/index.js
+++ score/index.js
@@ -3,6 +3,7 @@ const app = express();
 const store = require("store2");
 const port = process.env.PORT || 4000;
 const path = require("path");
+var userScore;
 
 app.use(express.static("www"));
 
@@ -12,6 +13,10 @@ app.get("/", (req, res) => {
 
 app.get("/score", (req, res) => {
   // Setting store key and data
+  /*fetch("http:localhost:8080/score").then(score => {
+    store.each()
+    score["score"]
+  })*/
   store.set("user", { name: "Adam", age: 34 });
   store.set("user1", { name: "user1", age: 35 });
   store.each(function (value, key) {
